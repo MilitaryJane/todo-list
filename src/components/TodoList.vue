f4a5dcf4d92d6136797fc8424ab757cbcd2f5e70
MilitaryJane <lyubovknyaginskaya@gmail.com>
2020-06-09 12:33:02 +0300

    Создание и рег. компонента TodoItem

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <ul>
-      <li>Какая-то задача</li>
+      <TodoItem />
     </ul>
   </div>
 </template>
 
 
 <script>
+import TodoItem from '@/components/TodoItem'
 export default {
-    
+  components: {
+    TodoItem
+  }
 }
 </script>
