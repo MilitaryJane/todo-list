63e3cb868619c6e23710238cb8a6aa03fb9cfcff
MilitaryJane <lyubovknyaginskaya@gmail.com>
2020-06-16 11:17:26 +0300

    редактирование задач кнопками

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,21 +1,36 @@
 <template>
 	<li>
 		<span v-bind:class="{done: item.completed}">
-			<input type="checkbox" v-on:change="isComplete">
+			<input type="checkbox"
+            v-if="showEdit"
+            v-on:change="isComplete">
 			<strong>{{ index + 1 }}</strong>
-			<span v-if="showEdit"
-            v-on:dblclick="toggleShow">
+			<span v-if="showEdit">
             {{ item.text | uppercase }}
       </span>
       <input class = "edit" type="text"
             v-else
             v-model="item.text"
-            v-on:contextmenu.prevent ="toggleShow"
-            v-on:keyup.enter="toggleShow">
+            v-on:keyup.enter="acceptItem"
+            v-on:keyup.esc="cancelEdit">
 		</span>
-		<button class="remove"
-						v-on:click="$emit ('remove-item', item.id)"
-		>&times;</button>
+    <div>
+      <button class="edit-button"
+              v-if="!item.completed && showEdit"
+              v-on:click="editItem">
+              &#9998;
+      </button>
+      <button class="accept-button"
+              v-else-if="!item.completed && !showEdit"
+              v-on:click="acceptItem">
+              &#9989;
+      </button>
+      <button class="remove-button"
+              v-on:click="$emit ('remove-item', item.id)">
+              &times;
+      </button>
+    </div>
+
 	</li>
 </template>
 
@@ -23,7 +38,8 @@
 export default {
 	data() {
 		return {
-      showEdit: true
+      showEdit: true,
+      oldValue: null
     }
 	},
 	// в props валидируем входящие параметры:
@@ -43,8 +59,21 @@ export default {
 		isComplete() {
 			this.item.completed = !this.item.completed;
     },
-    toggleShow() {
+    isShow() {
       this.showEdit = !this.showEdit;
+    },
+    editItem() {
+      this.isShow();
+      this.oldValue = this.item.text;
+    },
+    acceptItem() {
+      this.isShow();
+      this.oldValue = null;
+    },
+    cancelEdit() {
+      this.isShow();
+      this.item.text = this.oldValue;
+      this.oldValue = null;
     }
 	}
 }
@@ -59,8 +88,21 @@ export default {
 		margin-bottom: 1rem;
 	}
 
-	.remove {
+	.remove-button {
 		background: red;
+		color: #ffffff;
+		border-radius: 50%;
+		font-weight: 700;
+	}
+
+  .edit-button {
+		background: yellow;
+		color: #000000;
+		border-radius: 50%;
+		font-weight: 700;
+	}
+    .accept-button {
+		background: green;
 		color: #ffffff;
 		border-radius: 50%;
 		font-weight: 700;
